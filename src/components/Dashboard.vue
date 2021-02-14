83b3407f105e48bc7e24b6ad095c74c756a8aa10
xeozor <tinssa12@gmail.com>
2021-02-14 18:22:44 +0300

    fisrt commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Dashboard</div>
+          <div class="card-body">
+            <div v-if="user" class="alert alert-success" role="alert">You are logged in!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {    
+    ...mapGetters({
+      user: "user"
+    })
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from "./store"
+import router from "./routes/index"
+import firebase from 'firebase'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBHommFjXRf-iCZx0oG0ayWEXfqs-0ko-U",
+  authDomain: "bitmex-abuse.firebaseapp.com",
+  projectId: "bitmex-abuse",
+  storageBucket: "bitmex-abuse.appspot.com",
+  messagingSenderId: "510154972634",
+  appId: "1:510154972634:web:a1f174f1df50382750b4e3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
